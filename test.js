6717ba3912287e7c84913f3180729405735d657f
88potatoes <94770511+88potatoes@users.noreply.github.com>
2023-11-08 15:22:27 +1100

    add: https support

--- test.js
+++ test.js
@@ -1,10 +1,19 @@
+const fs = require('fs');
+const https = require('https');
 const express = require('express');
 const app = express();
+const port = 443;
 
 app.use("/", (req, res) => {
     res.send("muhahahaha");
 })
 
-app.listen(8001, () => {
-    console.log("im listening");
+const privateKey = fs.readFileSync('somepath', 'utf8');
+const certificate = fs.readFileSync('somepath', 'utf8');
+const credentials = {key: privateKey, cert: certificate};
+
+const httpsServer = https.createServer(credentials, app);
+
+httpsServer.listen(port, () => {
+    console.log("listening on port:", port);
 });
